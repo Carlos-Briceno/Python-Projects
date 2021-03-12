073285be831b962b57f064c8ae7cabde81a409f7
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-03-11 21:27:09 -0700

    push

--- Encapsulation_assignment.py
+++ Encapsulation_assignment.py
@@ -2,17 +2,24 @@
 # This is protected 
 
 class Secret:
+
     def __init__(self):
-        self.privateVar = 'Protected'
+        self._protectedVar = 0
+
+
+    def __init__(self):
+        self.__privateVar = 0
 
     def getPrivate(self):
-        print(self.privateVar)  # this is a new value "privateVar
+        print(self.__privateVar)  # this is a new value "privateVar
 
     def setPrivate(self, private):
-        self.privateVar = private
+        self.__privateVar = private
 
 
 obj = Secret()
+obj._protectedVar = ('Protected')
+print(obj._protectedVar)
 obj.getPrivate()
 obj.setPrivate('Private') # this will print out self.privateVar
 obj.getPrivate()
