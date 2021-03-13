d3318f4ba4dafe690d659419a7e332bbd8f5c910
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-03-12 20:05:23 -0700

    push

--- Abstraction_assignment.py
+++ Abstraction_assignment.py
@@ -1,3 +1,21 @@
 from abc import ABC, abstractmethod
 
-class 
+# this is a parent class
+class Credit(ABC):
+    def AmountToPay(self, amount):
+        print("Your amount to pay: ",amount)
+
+    @abstractmethod
+    def amountLeft(self, amount):
+        pass
+
+# here is the implement amountLeft function from its parent AmountToPay class
+class Paynow(Credit):
+    def amountLeft(self, amount):
+        print('Your amount paid of {} and your amount left to pay is $53.00 '.format(amount))
+
+obj = Paynow()
+obj.AmountToPay("$500.00")
+obj.amountLeft("$500.00")
+    
+    
