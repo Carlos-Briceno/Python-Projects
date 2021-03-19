9d4733bbf042829bc5b1e77568a6106e6a9a5346
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-03-18 19:53:44 -0600

    push

--- Web_page_generator.py
+++ Web_page_generator.py
@@ -1,7 +1,17 @@
-
+import subprocess
+import os
 
 # this will show in an html file when opened
+url = "D:/cxbri/Python-Projects/Web_page_generator.html"
+try: # should work on Windows
+    os.startfile(url)
+except AttributeError:
+    try: # should work on MacOS and most linux versions
+        subprocess.call(['open', url])
+    except:
+        print('Could not open URL')
 
+# this is where the html file is made 
 html_str = """
 <html>
     <body>
@@ -11,15 +21,10 @@ html_str = """
     </body>
 </html>
 """
-
-Html_file= open("Web_page_generator.html","w")
-Html_file.write(html_str)
-Html_file.close()
-
 # this is importing tkinter 
 import tkinter as tk
 root = tk.Tk()
-root.geometry("400x50")
+root.geometry("400x200")
 
 # this is defining setTextInput 
 def setTextInput(text):
@@ -30,6 +35,9 @@ textExample = tk.Entry(root)
 textExample.pack()
 # this is the button that will show up in tkinter as set 
 btnSet = tk.Button(root, height=1, width=10, text="Set", command=lambda:setTextInput("New Text"))
+Html_file= open("Web_page_generator.html","w")
+Html_file.write(html_str)
+Html_file.close()
 btnSet.pack()
 
 root.mainloop()
