f7715b4576707e68bf11b1d4da1fc422091c5358
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-03-21 00:37:58 -0600

    push

--- Web_page_generator.py
+++ Web_page_generator.py
@@ -1,15 +1,3 @@
-import subprocess
-import os
-
-# this will show in an html file when opened
-url = "D:/cxbri/Python-Projects/Web_page_generator.html"
-try: # should work on Windows
-    os.startfile(url)
-except AttributeError:
-    try: # should work on MacOS and most linux versions
-        subprocess.call(['open', url])
-    except:
-        print('Could not open URL')
 
 # this is where the html file is made 
 html_str = """
@@ -21,24 +9,37 @@ html_str = """
     </body>
 </html>
 """
+Html_file = open("Web_page_generator.html","w")
+Html_file.write(html_str)
+Html_file.close()
+
 # this is importing tkinter 
 import tkinter as tk
+
+import webbrowser
+
 root = tk.Tk()
 root.geometry("400x200")
 
 # this is defining setTextInput 
 def setTextInput(text):
-    textExample.delete(0,"end")
-    textExample.insert(0, text)
+    with open('Web_page_generator.html', 'w') as file:
+        file.write(f'<html><body><h1>{text}</h1></body></html>')
+        webbrowser.open('Web_page_generator.html')
+        
 
-textExample = tk.Entry(root)
-textExample.pack()
-# this is the button that will show up in tkinter as set 
-btnSet = tk.Button(root, height=1, width=10, text="Set", command=lambda:setTextInput("New Text"))
-Html_file= open("Web_page_generator.html","w")
-Html_file.write(html_str)
-Html_file.close()
+# this will show in an html file when opened
+url = "D:/cxbri/Python-Projects/Web_page_generator.html"
+webbrowser.open_new_tab(url)
+
+text = tk.Entry(root)
+text.pack()
+
+
+# this is the button that will show up in tkinter as set
+btnSet = tk.Button(root, height=1, width=10, text="Set", command=lambda:setTextInput(text.get()))
 btnSet.pack()
+
 
 root.mainloop()
 
